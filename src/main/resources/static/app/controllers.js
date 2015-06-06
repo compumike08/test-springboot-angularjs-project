b7550b8c6ac396738988f45d2006c024ad09f4d8
compumike08 <compumike08@users.noreply.github.com>
2015-06-06 06:53:50 -0400

    Add controllers.js file

---
+++ src/main/resources/static/app/controllers.js
@@ -0,0 +1,34 @@
+(function(angular){
+    var AppController = function($scope, Item) {
+        Item.query(function(response) {
+            if(response){
+                $scope.items = response;
+            }else{
+                $scope.items = [];
+            }
+        });
+
+        $scope.addItem = function(description){
+            new Item({
+                description: description,
+                checked: false
+            }).$save(function(item) {
+                $scope.items.push(item);
+            });
+            $scope.newItem = "";
+        };
+
+        $scope.updateItem = function(item){
+            item.$update();
+        };
+
+        $scope.deleteItem = function(item){
+            item.$remove(function(){
+                $scope.items.splice($scope.items.indexOf(item), 1);
+            });
+        };
+    };
+
+    AppController.$inject = ['$scope', 'Item'];
+    angular.module("myApp.controllers").controller("AppController", AppController);
+}(angular));
